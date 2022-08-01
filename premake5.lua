c8a13621c8dd9a4445e3e667adfa149f50d36093
Posmac Nicolai <nicolai.posmac@gmail.com>
2022-08-01 12:51:16 +0300

    premake

--- premake5.lua
+++ premake5.lua
@@ -1,9 +1,10 @@
 project "GLFW"
     kind "StaticLib"
     language "C"
+    staticruntime "on"
     
 	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
-    objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin/intermidiate/" .. outputdir .. "/%{prj.name}")
 
 	files
 	{
@@ -24,9 +25,7 @@ project "GLFW"
     }
     
 	filter "system:windows"
-        buildoptions { "-std=c11", "-lgdi32" }
         systemversion "latest"
-        staticruntime "On"
         
         files
         {
@@ -47,5 +46,11 @@ project "GLFW"
             "_GLFW_WIN32",
             "_CRT_SECURE_NO_WARNINGS"
 		}
-    filter { "system:windows", "configurations:Release" }
-        buildoptions "/MT"
+
+filter "configurations:Debug"
+    runtime "Debug"
+    symbols "on"
+
+filter "configurations:Release"
+    runtime "Release"
+    optimize "on"
